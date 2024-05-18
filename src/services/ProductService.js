41fbf93b442e02402b0626978cc95ed2846f8826
TranVu <tmv2k3@gmail.com>
2024-05-18 15:09:17 +0700

    first commit

---
+++ src/services/ProductService.js
@@ -0,0 +1,184 @@
+const Product = require("../models/ProductModel")
+
+const createProduct = (newProduct) => {
+    return new Promise(async (resolve, reject) => {
+        const { name, image, type, price, description, countInStock } = newProduct
+        try {
+            const checkProduct = await Product.findOne({
+                name: name
+            })
+            if (checkProduct !== null) {
+                resolve({
+                    status: 'ERR',
+                    message: 'The name of product is already'
+                })
+            }
+            const newProduct = await Product.create({
+                name,
+                image,
+                type,
+                price,
+                description,
+                countInStock
+            })
+            if (newProduct) {
+                resolve({
+                    status: 'OK',
+                    message: 'SUCCESS',
+                    data: newProduct
+                })
+            }
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+const updateProduct = (id, data) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const checkProduct = await Product.findOne({
+                _id: id
+            })
+            if (checkProduct === null) {
+                resolve({
+                    status: 'ERR',
+                    message: 'The product is not defined'
+                })
+            }
+
+            const updatedProduct = await Product.findByIdAndUpdate(id, data, { new: true })
+            resolve({
+                status: 'OK',
+                message: 'SUCCESS',
+                data: updatedProduct
+            })
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+const deleteProduct = (id) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const checkProduct = await Product.findOne({
+                _id: id
+            })
+            if (checkProduct === null) {
+                resolve({
+                    status: 'ERR',
+                    message: 'The product is not defined'
+                })
+            }
+
+            await Product.findByIdAndDelete(id)
+            resolve({
+                status: 'OK',
+                message: 'Delete product success',
+            })
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+const deleteManyProduct = (ids) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            await Product.deleteMany({ _id: ids })
+            resolve({
+                status: 'OK',
+                message: 'Delete product success',
+            })
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+const getDetailsProduct = (id) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const product = await Product.findOne({
+                _id: id
+            })
+            if (product === null) {
+                resolve({
+                    status: 'ERR',
+                    message: 'The product is not defined'
+                })
+            }
+
+            resolve({
+                status: 'OK',
+                message: 'SUCESS',
+                data: product
+            })
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+const getAllType = () => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const allType = await Product.distinct('type')
+            resolve({
+                status: 'OK',
+                message: 'Success',
+                data: allType,
+            })
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+const getAllProduct = (limit, page, filter) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const totalProduct = await Product.countDocuments()
+            let allProduct = []
+            if (filter) {
+                const label = filter[0];
+                const regex = new RegExp(filter[1], 'i');
+                const allObjectFilter = await Product.find({ [label]: { '$regex': regex } }).limit(limit).skip(page * limit)
+                resolve({
+                    status: 'OK',
+                    message: 'Success',
+                    data: allObjectFilter,
+                    total: totalProduct,
+                    pageCurrent: Number(page + 1),
+                    totalPage: Math.ceil(totalProduct / limit)
+                })
+            }
+            if (!limit) {
+                allProduct = await Product.find()
+            } else {
+                allProduct = await Product.find().limit(limit).skip(page * limit)
+            }
+            resolve({
+                status: 'OK',
+                message: 'Success',
+                data: allProduct,
+                total: totalProduct,
+                pageCurrent: Number(page + 1),
+                totalPage: Math.ceil(totalProduct / limit)
+            })
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+module.exports = {
+    createProduct,
+    updateProduct,
+    getDetailsProduct,
+    deleteProduct,
+    getAllProduct,
+    deleteManyProduct,
+    getAllType
+}
